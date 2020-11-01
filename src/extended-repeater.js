c724b2c037ecc53a4d27d671d1e7fab07d87b0de
snegurova <snegurova@gmail.com>
2020-11-01 13:30:24 +0200

    feat: implement repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let result = '';
+
+  createRepeatedStr = (string, repeat, separator) => {
+    if (string === null) {
+      string = 'null';
+    }
+    if (string || string === false) {
+      string = string.toString();
+    } else {
+      return '';
+    }
+
+    if (!repeat) {
+      return string;
+    }
+
+    let strArr = [];
+    for (let i = 0; i < repeat; i++) {
+      strArr.push(string);
+    }
+
+    return strArr.join(separator);
+  }
+
+  str += createRepeatedStr(options.addition, options.additionRepeatTimes,
+    options.additionSeparator || '|');
+
+  result += createRepeatedStr(str, options.repeatTimes,
+    options.separator || '+');
+
+  return result;
 };
-  
+
+// repeater(true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' })
